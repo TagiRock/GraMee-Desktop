357888f96e740b656ba20b934e46302a072fbc6e
Konatsup <konatsukomatsu@gmail.com>
2017-12-17 17:22:06 +0900

    #Installed RxJS

--- src/domain/usecase/ClassUseCase.ts
+++ src/domain/usecase/ClassUseCase.ts
@@ -1,9 +1,13 @@
-
-
+import { Observable, Observer } from "rxjs";
 export class ClassUseCase {
 
-    public createClass() {
-        
+    // public createClass() {
+    //     const a: Observable<string> = Observable.create(
+    //         (emitter: Observer<string>) => {
+    //            emitter.next("")
+    //            emitter.error("")
+    //         }
+    //     );
 
-    }
+    // }
 }
