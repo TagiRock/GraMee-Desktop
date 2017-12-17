ce9d9db9bd4e9acb5380c97f15a5618a006038ba
youta1119 <dev.youta1119@gmail.com>
2017-12-17 15:54:52 +0900

    Element-UI削除

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import VueRouter from "vue-router";
 import Vuetify from "vuetify";
 import Vuex from "vuex";
 import App from "components/App.vue";
-import Element from "element-ui";
 import "vue-material-design-icons-plugin";
-import locale from "element-ui/lib/locale/lang/ja";
 import store from "./store";
 import router from "./router";
-Vue.use(Element, { locale });
 
 /* tslint:disable:no-unused-expression*/
 new Vue({
@@ -17,5 +14,5 @@ new Vue({
   template: "<App/>",
   store,
   router,
-  components: {App}
+  components: { App }
 });
