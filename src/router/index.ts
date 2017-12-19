c48e7aa947f6d1d18aeaec59b07dd9cbb2ce662b
youta1119 <dev.youta1119@gmail.com>
2017-12-19 23:46:29 +0900

    ヘッダーとサイドバー

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,15 +4,25 @@ import Message from "components/message/Message.vue";
 import School from "components/school/School.vue";
 import Setting from "components/setting/Setting.vue";
 import Home from "components/home/Home.vue";
+import Login from "components/login/Login.vue";
 
 Vue.use(VueRouter);
-const routes = [
-    { path: "/", component: School },
-    { path: "message", component: Message },
-    { path: "home", component: Home },
-    { path: "setting", component: Setting }
-  ];
-export default new VueRouter({
-    mode: "history",
-    routes: routes
-  });
+const routes: = [
+  { path: "/", component: School },
+  { path: "message", component: Message },
+  { path: "home", component: Home },
+  { path: "setting", component: Setting },
+  { path: "login", component: }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+
+  }
+});
+export default router;
